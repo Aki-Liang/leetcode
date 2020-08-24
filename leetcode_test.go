86403f69efddd07c487fb03daae1664518cd3d4c
Aki-Liang <11916467@qq.com>
2020-08-24 20:11:46 +0800

    add no11

--- leetcode_test.go
+++ leetcode_test.go
@@ -74,3 +74,8 @@ func Test_merge(t *testing.T) {
 	// nums2 = [2,5,6],       n = 3
 	merge([]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3)
 }
+
+func Test_maxArea(t *testing.T) {
+	res := maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7})
+	fmt.Println(res)
+}
