1f45d28499e8f22b817bfc105072a865ecaf0b90
Aki-Liang <11916467@qq.com>
2020-10-14 10:50:56 +0800

    ADD 367 547 ， update 200

--- no200.go
+++ no200.go
@@ -32,33 +32,95 @@ package leetcode
 // 链接：https://leetcode-cn.com/problems/number-of-islands
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
+func find(parents []int, i int) int {
+	root := i
+	for parents[root] != root {
+		root = parents[root]
+	}
+
+	for parents[i] != i {
+		i, parents[i] = parents[i], root
+	}
+
+	return i
+}
+
+func union(parents []int, i, j int) {
+	ri := find(parents, i)
+	rj := find(parents, j)
+	if ri != rj {
+		parents[ri] = rj
+	}
+}
+
 func numIslands(grid [][]byte) int {
-	count := 0
-	for line := range grid {
-		for column := range grid[line] {
-			if '0' == grid[line][column] { //找到陆地
+	if len(grid) == 0 || len(grid[0]) == 0 {
+		return 0
+	}
+	n, m := len(grid), len(grid[0])
+	parents := make([]int, m*n)
+	for i := 0; i < m*n; i++ {
+		parents[i] = i
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if grid[i][j] == '0' {
 				continue
 			}
-			count++
-			dfs(grid, line, column)
+
+			pos := i*m + j
+			if j+1 < m && grid[i][j+1] == '1' {
+				//如果右边是陆地，合并
+				union(parents, pos, pos+1)
+			}
+			if i+1 < n && grid[i+1][j] == '1' {
+				//如果下边是陆地，合并
+				union(parents, pos, pos+m)
+
+			}
 		}
 	}
 
-	return count
+	islands := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if grid[i][j] == '1' && parents[i*m+j] == i*m+j {
+				islands++
+			}
+		}
+	}
+
+	return islands
 }
 
-func dfs(grid [][]byte, line, column int) {
-	//找到相连的陆地，然后搞沉没
-	if line < 0 || line >= len(grid) || column < 0 || column >= len(grid[line]) {
-		return
-	}
-	if grid[line][column] != '1' {
-		//没有陆地了
-		return
-	}
-	grid[line][column] = '0'
-	dfs(grid, line+1, column)
-	dfs(grid, line-1, column)
-	dfs(grid, line, column+1)
-	dfs(grid, line, column-1)
-}
+// func numIslands(grid [][]byte) int {
+// 	count := 0
+// 	for line := range grid {
+// 		for column := range grid[line] {
+// 			if '0' == grid[line][column] { //找到陆地
+// 				continue
+// 			}
+// 			count++
+// 			dfs(grid, line, column)
+// 		}
+// 	}
+
+// 	return count
+// }
+
+// func dfs(grid [][]byte, line, column int) {
+// 	//找到相连的陆地，然后搞沉没
+// 	if line < 0 || line >= len(grid) || column < 0 || column >= len(grid[line]) {
+// 		return
+// 	}
+// 	if grid[line][column] != '1' {
+// 		//没有陆地了
+// 		return
+// 	}
+// 	grid[line][column] = '0'
+// 	dfs(grid, line+1, column)
+// 	dfs(grid, line-1, column)
+// 	dfs(grid, line, column+1)
+// 	dfs(grid, line, column-1)
+// }
